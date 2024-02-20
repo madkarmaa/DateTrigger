04313ae042f2b2e1268c39f26b6509ed075c2abf
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2024-02-20 18:56:37 +0100

    feat: added colors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,21 +7,21 @@ const currentYear = new Date().getFullYear();
 export const DAY_IN_MINUTES = 1440;
 export const DEFAULT_EVENTS = {
     NEW_YEAR: new EventBuilder(new Date(currentYear, 1, 1), DAY_IN_MINUTES, () => {
-        changeColorVars();
+        changeColorVars('#e6e8fe', '#010215', '#7082f9', '#960670', '#f62e65');
     }),
     CHRISTMAS: new EventBuilder(new Date(currentYear, 11, 25), DAY_IN_MINUTES, () => {
-        changeColorVars();
+        changeColorVars('#fdfcfc', '#030202', '#0ee768', '#f30404', '#ffffff');
     }),
     VALENTINES_DAY: new EventBuilder(new Date(currentYear, 1, 14), DAY_IN_MINUTES, () => {
-        changeColorVars();
+        changeColorVars('#efd6f9', '#18051f', '#dc8fef', '#93166c', '#e34190');
     }),
     HALLOWEEN: new EventBuilder(new Date(currentYear, 9, 31), DAY_IN_MINUTES, () => {
-        changeColorVars();
+        changeColorVars('#f2eeed', '#0c0705', '#df9d87', '#923112', '#fd490e');
     }),
     EASTER: new EventBuilder(getEaster(currentYear), DAY_IN_MINUTES, () => {
-        changeColorVars();
+        changeColorVars('#f7defc', '#09010c', '#dc7df2', '#f60087', '#6cdeb8');
     }),
     APRIL_FOOLS: new EventBuilder(new Date(currentYear, 3, 1), DAY_IN_MINUTES, () => {
-        changeColorVars();
+        changeColorVars('#ceeafe', '#000910', '#7ec2fc', '#9504af', '#fa28a7');
     }),
 };
