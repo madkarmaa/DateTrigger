178d1fbaede75f939fcb0efb45a94564c20a5bd0
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2024-02-21 22:40:52 +0100

    fix: fixed imports

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import { EventBuilder } from './utils/eventBuilder.js';
+import { DateTriggerEvent } from './utils/dateTriggerEvent.js';
 import { EventsHandler } from './utils/eventsHandler.js';
 import { DAY_IN_MINUTES, DEFAULT_EVENTS } from './utils/constants.js';
 
 // EXPOSE TO BROWSER
 if (typeof window !== 'undefined') {
     window['EventsHandler'] = EventsHandler;
-    window['EventBuilder'] = EventBuilder;
+    window['DateTriggerEvent'] = DateTriggerEvent;
     window['DAY_IN_MINUTES'] = DAY_IN_MINUTES;
     window['DEFAULT_EVENTS'] = DEFAULT_EVENTS;
 }
 
-export { EventsHandler, EventBuilder, DAY_IN_MINUTES, DEFAULT_EVENTS };
+export { EventsHandler, DateTriggerEvent, DAY_IN_MINUTES, DEFAULT_EVENTS };
