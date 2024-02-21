136a357864ba5ec1c57f56d96a0dd95f218b59c8
MadKarma <100418457+madkarmaa@users.noreply.github.com>
2024-02-21 21:32:04 +0100

    feat: added event duration logic

--- src/utils/eventBuilder.js
+++ src/utils/eventBuilder.js
@@ -1,18 +1,19 @@
 export class EventBuilder {
     /**
      * Helper class for creating events
-     * @param {Date} date When the event should start
+     * @param {Date} startDate When the event should start
      * @param {number} durationInMinutes How long the event should last for
      * @param {Function} handler The function to call when the event triggers
      */
-    constructor(date, durationInMinutes, handler) {
-        if (!(date instanceof Date)) throw new TypeError('date must be of type Date');
+    constructor(startDate, durationInMinutes, handler) {
+        if (!(startDate instanceof Date)) throw new TypeError('startDate must be of type Date');
         if (typeof durationInMinutes !== 'number') throw new TypeError('durationInMinutes must be a number');
         if (typeof handler !== 'function') throw new TypeError('handler must be a function');
 
-        this.date = date;
+        this.startDate = startDate;
         this.durationInMinutes = durationInMinutes;
         this.handler = handler;
+        this.endDate = new Date(this.startDate.getTime() + durationInMinutes * 60000);
 
         Object.freeze(this);
     }
--- src/utils/eventsHandler.js
+++ src/utils/eventsHandler.js
@@ -5,15 +5,13 @@ export class EventsHandler {
     /**
      * Trigger events on specific dates
      * @param {Record<any, EventBuilder> | undefined} customEvents An object of EventBuilder class instances. **NOTE**: the key is considered the name of the event
-     * @param {boolean} useEventsPreset Also add the default set of events
      */
     constructor(customEvents = { ...DEFAULT_EVENTS }) {
         if (typeof customEvents !== 'object') throw new TypeError('customEvents must be an object');
         if (Object.values(customEvents).some((e) => !(e instanceof EventBuilder)))
             throw new TypeError('Every entry in customEvents must be an instance of EventBuilder');
 
-        // this.today = new Date();
-        this.today = new Date(2024, 0, 1);
+        this.now = new Date();
         this.events = { ...customEvents };
 
         if (!EventsHandler.instance) EventsHandler.instance = this;
@@ -27,12 +25,32 @@ export class EventsHandler {
     #run() {
         for (const evName in this.events) {
             const event = this.events[evName];
-            if (this.#areSameDate(event.date, this.today)) event.handler();
+            if (this.#areSameDate(event.startDate, this.now) && this.#isEventActive(event)) event.handler();
         }
     }
 
-    // https://stackoverflow.com/a/8394028
+    /**
+     * https://stackoverflow.com/a/8394028
+     * @param {Date} d1
+     * @param {Date} d2
+     * @returns {boolean}
+     */
     #areSameDate(d1, d2) {
         return d1.getMonth() === d2.getMonth() && d1.getDate() === d2.getDate();
+    }
+
+    /**
+     * Check if an event is active
+     * @param {EventBuilder} event The event to check if it's active
+     * @returns {boolean}
+     */
+    #isEventActive(event) {
+        if (!(event instanceof EventBuilder)) throw new TypeError('event must be an instance of EventBuilder');
+
+        const now = Date.parse(new Date());
+        const eventStartTime = Date.parse(event.startDate);
+        const eventEndTime = Date.parse(event.endDate);
+
+        return now >= eventStartTime && now <= eventEndTime;
     }
 }
