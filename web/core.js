9bf34db2a65b5db75cb067fbdc3ece050b456c40
GitHub Action <action@github.com>
2024-12-25 18:55:41 +0000

    build: build web lib version

--- web/core.js
+++ web/core.js
@@ -1,77 +1,66 @@
-class DateTriggerEvent {
-    /**
-     * Helper class for creating events
-     * @param startDate When the event should start
-     * @param durationInMinutes How long the event should last for
-     * @param handler The function to call when the event triggers
-     */
-    constructor(startDate, durationInMinutes, handler) {
-        if (!(startDate instanceof Date))
-            throw new TypeError('startDate must be of type Date');
-        if (typeof durationInMinutes !== 'number')
-            throw new TypeError('durationInMinutes must be a number');
-        if (typeof handler !== 'function')
-            throw new TypeError('handler must be a function');
-        this.startDate = startDate;
-        this.durationInMinutes = durationInMinutes;
-        this.handler = handler;
-        this.endDate = new Date(this.startDate.getTime() + durationInMinutes * 60000);
-        Object.freeze(this);
-    }
+const isDateInPeriod = (date, period) => {
+    return date >= period.from && date <= period.to;
+};
+const sameDay = (date) => {
+    return { from: date, to: date };
+};
+const getEasterDateOfYear = (year) => {
+    const a = year % 19, b = Math.floor(year / 100), c = year % 100, d = Math.floor(b / 4), e = b % 4, f = Math.floor((b + 8) / 25), g = Math.floor((b - f + 1) / 3), h = (19 * a + b - d - g + 15) % 30, i = Math.floor(c / 4), k = c % 4, L = (32 + 2 * e + 2 * i - h - k) % 7, m = Math.floor((a + 11 * h + 22 * L) / 451), month = Math.floor((h + L - 7 * m + 114) / 31), day = ((h + L - 7 * m + 114) % 31) + 1;
+    return new Date(year, month - 1, day);
+};
+function assertIsPeriod(value) {
+    if (!value ||
+        typeof value !== 'object' ||
+        !value.from ||
+        !(value.from instanceof Date) ||
+        !value.to ||
+        !(value.to instanceof Date))
+        throw new Error('Expected a Period object');
+}
+function assertIsDateEvent(value) {
+    if (!value ||
+        typeof value !== 'object' ||
+        !value.name ||
+        typeof value.name !== 'string' ||
+        !value.period ||
+        !value.execute ||
+        typeof value.execute !== 'function')
+        throw new Error('Expected a DateEvent object');
+    // https://stackoverflow.com/a/72689922
+    assertIsPeriod(value.period);
 }
 
-class DateTriggerEventHandler {
-    /**
-     * Trigger events on specific dates
-     * @param customEvents A list of DateTriggerEvent class instances
-     */
-    constructor(customEvents = []) {
-        if (typeof customEvents !== 'object')
-            throw new TypeError('customEvents must be an object');
-        if (customEvents.some((e) => !(e instanceof DateTriggerEvent)))
-            throw new TypeError('Every entry in customEvents must be an instance of DateTriggerEvent');
-        this.now = new Date();
-        this.events = [...customEvents];
-        if (!DateTriggerEventHandler.instance)
-            DateTriggerEventHandler.instance = this;
-        Object.freeze(this);
+class EventsHandler {
+    static instance = null;
+    events;
+    verbose;
+    constructor(events, verbose = false) {
+        events.forEach(assertIsDateEvent);
+        this.events = [...events];
+        this.verbose = verbose;
+        if (!EventsHandler.instance)
+            EventsHandler.instance = this;
         this.run();
-        return DateTriggerEventHandler.instance;
+        return EventsHandler.instance;
     }
     run() {
-        Object.values(this.events).forEach((event) => {
-            if (this.areSameDate(event.startDate, this.now) && this.isEventActive(event))
-                event.handler();
+        this.events.forEach((event) => {
+            const today = new Date();
+            if (!isDateInPeriod(today, event.period))
+                return;
+            if (this.verbose)
+                console.log(`Event "${event.name}" executed`);
+            event.execute();
         });
     }
-    /**
-     * https://stackoverflow.com/a/8394028
-     */
-    areSameDate(d1, d2) {
-        return d1.getMonth() === d2.getMonth() && d1.getDate() === d2.getDate();
-    }
-    /**
-     * Check if an event is active
-     * @param event The event to check if it's active
-     */
-    isEventActive(event) {
-        if (!(event instanceof DateTriggerEvent))
-            throw new TypeError('event must be an instance of DateTriggerEvent');
-        const now = Date.parse(new Date().toString());
-        const eventStartTime = Date.parse(event.startDate.toString());
-        const eventEndTime = Date.parse(event.endDate.toString());
-        return now >= eventStartTime && now <= eventEndTime;
-    }
 }
-
-const DAY_IN_MINUTES = 1440;
 
 // EXPOSE TO BROWSER
 if (typeof window !== 'undefined') {
     // https://stackoverflow.com/a/38964459
-    window['DateTriggerEventHandler'] = DateTriggerEventHandler;
-    window['DateTriggerEvent'] = DateTriggerEvent;
-    window['DAY_IN_MINUTES'] = DAY_IN_MINUTES;
+    window['EventsHandler'] = EventsHandler;
+    window['getEasterDateOfYear'] = getEasterDateOfYear;
+    window['sameDay'] = sameDay;
 }
 
-export { DAY_IN_MINUTES, DateTriggerEvent, DateTriggerEventHandler };
+export { EventsHandler, getEasterDateOfYear, sameDay };
